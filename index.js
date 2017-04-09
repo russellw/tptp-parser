a776834a895d68f9738553ad2abb8b02cb610a35
Russell Wallace <russell.wallace@gmail.com>
2017-04-09 02:26:52 +0100

    vars

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 'use strict'
 var fs = require('fs')
 
-// Tokenizer
-var file
-var i
-var text
-var tok
-
-function err(msg) {
-	var loc = location()
-	msg += ' (' + loc.line + ':' + loc.column + ')'
-	var e = new SyntaxError(msg)
-	e.file = file
-	e.loc = loc
-	e.pos = i
-	e.raisedAt = i
-	throw e
+function get(m, key) {
+	while (m) {
+		if (m.key === key)
+			return m.val
+		m = m.parent
+	}
 }
 
 function isalnum(c) {
@@ -40,6 +31,31 @@ function islower(c) {
 
 function isupper(c) {
 	return 'A' <= c && c <= 'Z'
+}
+
+function put(m, key, val) {
+	return {
+		key,
+		parent: m,
+		val,
+	}
+}
+
+// Tokenizer
+var file
+var i
+var text
+var tok
+
+function err(msg) {
+	var loc = location()
+	msg += ' (' + loc.line + ':' + loc.column + ')'
+	var e = new SyntaxError(msg)
+	e.file = file
+	e.loc = loc
+	e.pos = i
+	e.raisedAt = i
+	throw e
 }
 
 function lex() {
@@ -213,27 +229,39 @@ function location() {
 	}
 }
 
+var free
+
 // Parser
 var functions
 
 function annotated_formula() {
 	lex()
+
+	// Name
 	expect('(')
 	var nm = name()
+
+	// Role
 	expect(',')
 	var role = formula_role()
+
+	// Formula
 	expect(',')
+	free = new Map()
 	formula()
+
+	// Annotations
+	// End
 	expect(')')
 	expect('.')
 }
 
-function defined_term() {
+function defined_term(bound) {
 	switch (tok) {
 	case '$difference':
-		return defined_term_arity('-', 2)
+		return defined_term_arity(bound, '-', 2)
 	case '$distinct':
-		var args = term_args()
+		var args = term_args(bound)
 		var clauses = []
 		for (var i = 0; i < args.length; i++)
 			for (var j = 0; j < i; j++)
@@ -254,32 +282,32 @@ function defined_term() {
 			val: false,
 		}
 	case '$greater':
-		return defined_term_arity('>', 2)
+		return defined_term_arity(bound, '>', 2)
 	case '$greatereq':
-		return defined_term_arity('>=', 2)
+		return defined_term_arity(bound, '>=', 2)
 	case '$less':
-		return defined_term_arity('<', 2)
+		return defined_term_arity(bound, '<', 2)
 	case '$lesseq':
-		return defined_term_arity('<=', 2)
+		return defined_term_arity(bound, '<=', 2)
 	case '$product':
-		return defined_term_arity('*', 2)
+		return defined_term_arity(bound, '*', 2)
 	case '$quotient':
-		return defined_term_arity('/', 2)
+		return defined_term_arity(bound, '/', 2)
 	case '$sum':
-		return defined_term_arity('+', 2)
+		return defined_term_arity(bound, '+', 2)
 	case '$true':
 		return {
 			op: 'const',
 			val: true,
 		}
 	case '$uminus':
-		return defined_term_arity('-', 1)
+		return defined_term_arity(bound, '-', 1)
 	}
 	err('Unknown term')
 }
 
-function defined_term_arity(op, arity) {
-	var args = term_args()
+function defined_term_arity(bound, op, arity) {
+	var args = term_args(bound)
 	if (args.length !== arity)
 		err('Expected ' + arity + ' arguments')
 	return {
@@ -300,18 +328,18 @@ function expect(k) {
 		err("Expected '" + k + "'")
 }
 
-function formula() {
-	var args = [unitary_formula()]
+function formula(bound) {
+	var args = [unitary_formula(bound)]
 	var op = tok
 	switch (tok) {
 	case '&':
 	case '|':
 		while (eat(op))
-			args.push(unitary_formula())
+			args.push(unitary_formula(bound))
 		break
 	case '<=':
 		lex()
-		args.unshift(unitary_formula())
+		args.unshift(unitary_formula(bound))
 		op = '=>'
 		break
 	case '<=>':
@@ -320,7 +348,7 @@ function formula() {
 	case '~&':
 	case '~|':
 		lex()
-		args.push(unitary_formula())
+		args.push(unitary_formula(bound))
 		break
 	default:
 		return args[0]
@@ -331,8 +359,8 @@ function formula() {
 	}
 }
 
-function infix_unary() {
-	var a = term()
+function infix_unary(bound) {
+	var a = term(bound)
 	switch (tok) {
 	case '!=':
 	case '=':
@@ -341,7 +369,7 @@ function infix_unary() {
 		return {
 			args: [
 				a,
-				term(),
+				term(bound),
 			],
 			op,
 		}
@@ -369,10 +397,10 @@ function parse(t, f) {
 		}
 }
 
-function term() {
+function term(bound) {
 	switch (tok[0]) {
 	case '$':
-		return defined_term()
+		return defined_term(bound)
 	case "'":
 	case 'a':
 	case 'b':
@@ -400,35 +428,73 @@ function term() {
 	case 'x':
 	case 'y':
 	case 'z':
-		return plain_term()
+		return plain_term(bound)
+	case 'A':
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'E':
+	case 'F':
+	case 'G':
+	case 'H':
+	case 'I':
+	case 'J':
+	case 'K':
+	case 'L':
+	case 'M':
+	case 'N':
+	case 'O':
+	case 'P':
+	case 'Q':
+	case 'R':
+	case 'S':
+	case 'T':
+	case 'U':
+	case 'V':
+	case 'W':
+	case 'X':
+	case 'Y':
+	case 'Z':
+		var a = get(bound, tok)
+		if (a)
+			return a
+		a = free.get(tok)
+		if (a)
+			return a
+		a = {
+			name: tok,
+			op: 'var',
+		}
+		free.set(tok, a)
+		return a
 	}
 	err('Syntax error')
 }
 
-function term_args() {
+function term_args(bound) {
 	expect(')')
-	var a = [term()]
+	var a = [term(bound)]
 	while (eat(','))
-		a.push(term())
+		a.push(term(bound))
 	expect(')')
 	return a
 }
 
-function unitary_formula() {
+function unitary_formula(bound) {
 	switch (tok) {
 	case '(':
 		lex()
-		var a = formula()
+		var a = formula(bound)
 		expect(')')
 		return a
 	case '~':
 		lex()
 		return {
-			args: [unitary_formula()],
+			args: [unitary_formula(bound)],
 			op: '~',
 		}
 	}
-	return infix_unary()
+	return infix_unary(bound)
 }
 
 exports.parse = parse
